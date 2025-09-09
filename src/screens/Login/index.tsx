197ceb852ba82329403d6d9a93a86c4fb7455ca5
Marcelo Galdino <marcelo.oliveira@gazin.com.br>
2025-09-09 06:18:14 -0300

    feat: project initialized

---
+++ src/screens/Login/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View} from "react-native"
+
+export const Login = () => {
+  return (
+    <View className="flex-1 justify-center items-center bg-white">
+      <Text>Tela de login</Text>
+    </View>
+  )
+}
