d61f1b7f07d9e7665d5e34547e805a5512d44f46
Marcelo Galdino <marcelo.oliveira@gazin.com.br>
2025-10-30 21:20:28 -0300

    feat: Loading screen

---
+++ src/screens/Loading/index.tsx
@@ -0,0 +1,38 @@
+import { useAuthContext } from "@/Context/auth.context";
+import { colors } from "@/shared/colors";
+import { FC, useEffect } from "react";
+import { ActivityIndicator, Image } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+interface LoadingParams {
+  setLoading: (value: boolean) => void;
+}
+
+export const Loading: FC<LoadingParams> = ({ setLoading }) => {
+  const { restoreUserSession, handleLogout } = useAuthContext();
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const user = await restoreUserSession();
+
+        if (!user) {
+          await handleLogout();
+        }
+      } catch (error) {
+        await handleLogout();
+      } finally {
+        setLoading(false);
+      }
+    })();
+  }, []);
+
+  return (
+    <SafeAreaView className="bg-background-primary items-center justify-center flex-1">
+      <>
+        <Image className="h-[48]px" source={require("@/assets/Logo.png")} />
+        <ActivityIndicator color={colors.white} className="mt-20" />
+      </>
+    </SafeAreaView>
+  );
+};
